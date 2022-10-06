ceb84381fc04657a244bb997c4dd995394a4a264
Inna Batalova <batalovainna97@gmail.com>
2022-10-06 17:06:54 +0300

    Create: added scheme

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import "./sass/style.scss";
+
+import boxLeft from "./img/icon-box-left.svg";
+import boxRight from "./img/icon-box-right.svg";
+
+const leftImage = document.getElementById("boxLeft");
+const rightImage = document.getElementById("boxRight");
+
+leftImage.src = boxLeft;
+rightImage.src = boxRight;
